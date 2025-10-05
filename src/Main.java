93bde872f36cea0b1199f8fd74d95a3b3d1e73b5
Angela <angelayourant@nycstudents.net>
2025-10-05 13:59:27 -0400

    Added the method makeAppointment and test cases

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        boolean[][] schedule = new boolean[8][60];
-        AppointmentBook a = new AppointmentBook(schedule);
-        for(int i = 10; i < 15; i++) schedule[1][i] = true;
+        boolean[][] schedule = new boolean[8][60]; //creating a 2D boolean array with 8 rows (0..8 periods) & 60 columns (0...59 minutes) to make the period 2 table
+        AppointmentBook a = new AppointmentBook(schedule); //initialize an appointmentbook object and pass the schedule (array) into it
+
+//test case 1
+
+        for(int i = 10; i < 15; i++) schedule[1][i] = true; //schedule[1] means index 1, aka period 2. We set true for int i=10 until i<15 is false
         for(int i = 30; i < 45; i++) schedule[1][i] = true;
         for(int i = 50; i < 60; i++) schedule[1][i] = true;
         a.printPeriod(2);
-//       a.printPeriod(3);
-        a.findFreeBlock(2,30);
-        a.findFreeBlock(2,9);
-        a.findFreeBlock(2,20);
+
+        System.out.println(a.findFreeBlock(2,15)); //returns 30 to indicate that a 15-minute block in period 2, starting with minute 30, is available.
+        System.out.println(a.findFreeBlock(2,9)); //whenever there are multiple blocks that satisfy the requirement, the earliest starting minute is returned.
+        System.out.println(a.findFreeBlock(2,20)); //would return -1, since no 20-minute block of available minutes exists in period 2.
+        AppointmentBook b = new AppointmentBook(schedule);
+
+//test case 2
+        for (int i = 25; i < 30; i++) schedule [1][i] = true;
+        for (int i = 0; i < 15; i++) schedule [2][i] = true;
+        for (int i = 41; i < 60; i++) schedule [2][i] = true;
+        for (int i = 5; i < 30; i++) schedule [3][i] = true;
+        for (int i = 44; i < 60; i++) schedule [3][i] = true;
+        int period = 2;
+        while (period < 5) {
+            System.out.println("Period: " + period);
+            b.printPeriod(period);
+            period++;
+        }
+        b.printPeriod(4);
+        System.out.println(b. makeAppointment(2, 4, 22));
+        b.printPeriod(4);
+
+        System.out.println(b. makeAppointment(3, 4, 3));
+        b.printPeriod(3);
+
+        System.out.println(b. makeAppointment(2, 4, 30));
     }
+
 }
